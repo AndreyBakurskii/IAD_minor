9facbb38a33144397318b11221ca3375e5b8b5b2
Andrey <bakurskii2001@gmail.com>
2020-11-02 17:03:05 +0300

    top version task3.py

--- Homeworks/hw5/task3.py
+++ Homeworks/hw5/task3.py
@@ -1,25 +1,22 @@
-def possible_strings(string: str) -> list:
-    """
-    Создает всевозможные строки из элементов строки string путем перестановки
-    :param string: искомая строка
-    :return: список всевозможных строк из элементов string
-    """
-
-    if len(string) == 2:
-        return [string, string[::-1]]
-
-    if len(string) == 1:
-        return [string]
+def possible_strings(string: str, len_return_str: int, repeat: bool) -> list:
+    if len_return_str == 1:
+        return [char for char in string]
 
     result = []
 
-    for ind, elem in enumerate(string):
-        result.extend([elem + s for s in possible_strings(string[:ind:] + string[ind + 1:])])
+    if repeat:
+        for ind, elem in enumerate(string):
+            result.extend([elem + s for s in possible_strings(string, len_return_str - 1, repeat)])
 
-    # убираем повторяющиеся строки и возвращаем результат функции
-    return [s for s in set(result)]
+    else:
+        for ind, elem in enumerate(string):
+            result.extend([elem + s for s in possible_strings(string[:ind] + string[ind + 1:], len_return_str - 1, repeat)])
+
+    return [elem for elem in set(result)]
 
 
 STRING = "hello"
+LEN_RETURN_STR = len(STRING)
+REPEAT = False
 
-print(*possible_strings(STRING))
+print(*possible_strings(STRING, LEN_RETURN_STR, REPEAT))
