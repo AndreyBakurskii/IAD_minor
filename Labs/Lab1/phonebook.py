4041f9ff9ba679f204ad63b82c01060ab86b3b78
Andrey <bakurskii2001@gmail.com>
2020-11-18 16:15:32 +0300

    update and fix some methods in class Phonebook

--- Labs/Lab1/phonebook.py
+++ Labs/Lab1/phonebook.py
@@ -1,4 +1,5 @@
 import Labs.Lab1.record as rec
+import Labs.Lab1.additional_functions as add_func
 
 
 class Phonebook:
@@ -12,11 +13,31 @@ class Phonebook:
     def append(self, record: rec.Record):
         self.list_records.append(record)
 
-    def delete_records(self, func=None):
-        self.list_records = list(filter(lambda x: True if not func(x) else False, self.list_records))
+    def delete_records(self, comparator=None):
+        self.list_records = list(filter(lambda x: True if not comparator(x) else False, self.list_records))
 
-    def get_records(self, func=None) -> rec.SetRecords:
-        return rec.SetRecords(list(filter(func, self.list_records)))
+    def get_record(self, comparator=None) -> rec.Record or None:
+        for record in self.list_records:
+            if comparator(record):
+                return record
+        return None
+
+    def get_records(self, comparator=None) -> rec.SetRecords or None:
+        list_records = list(filter(comparator, self.list_records))
+
+        return rec.SetRecords(list_records) if list_records else None
+
+
+def check_same_record(phonebook: Phonebook, record: rec.Record) -> True or None:
+    comparator = lambda x: x.firstname == record.firstname and x.lastname == record.lastname
+
+    existed_record = phonebook.get_record(comparator)
+    if existed_record is not None:
+        add_func.print_warning_record("Record with your firstname and lastname has already existed in phonebook.\n" +
+                                      "Record: " + str(existed_record) + "\n")
+        return
+
+    return True
 
 
 if __name__ == '__main__':
@@ -26,6 +47,8 @@ if __name__ == '__main__':
     phonebook.append(rec.Record("andrey", "bakur", "89200141489", "30.03.2001"))
     phonebook.append(rec.Record("ivan", "bakur", "89124124124", "15.02.1993"))
     phonebook.append(rec.Record("Diana", "Utenkova", "89564395671", "28.05.2002"))
+
+    print(phonebook.get_records(lambda x: x.firstname == "A"))
 
     print(phonebook)
 
